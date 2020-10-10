0479789d191d2b7c0ec2e581e8048af4d19eb8ca
JChuayta <34210605+JChuayta@users.noreply.github.com>
2020-10-10 09:47:50 -0600

    kj
    jjknjk

--- lib/main.dart
+++ lib/main.dart
@@ -41,21 +41,20 @@ class _MiHomeState extends State<MiHome> {
   TextEditingController contenidoController = new TextEditingController();
 
   //instancias de los patrones de dise;o
-  List<Recordatorio> misNotas = [ ];
-  FabricaWidgets fabrica = new MaterialWidget();//INSTANCIA DEL PATRON FABRICA ABSTRACTA
-  Recordatorio _recordatorio;  // INSANCIA DEL PATRON FABRICA ABSTRACTA Y PROTOTIPE
-
+  List<Recordatorio> misNotas = [];
+  FabricaWidgets fabrica =
+      new MaterialWidget(); //INSTANCIA DEL PATRON FABRICA ABSTRACTA
+  Recordatorio
+      _recordatorio; // INSANCIA DEL PATRON FABRICA ABSTRACTA Y PROTOTIPE
+  Color colr;
   void _showAlertDialog() {
     showDialog<void>(
       context: context,
       barrierDismissible: false,
       builder: (BuildContext _) {
-        return  _recordatorio.Alert(context);
+        return _recordatorio.Alert(context);
       },
     );
-  }
-  getColorAplicandoEstrategia(ColorEstrategy estrategy){
-    return estrategy.getColor();
   }
 
   Future<void> _selectDate(BuildContext context) async {
@@ -80,11 +79,11 @@ class _MiHomeState extends State<MiHome> {
     // TODO: implement initState
     super.initState();
     _recordatorio = fabrica.createRecordatorio();
-    Timer.periodic(Duration(seconds: 5), (nada) {
+    /*Timer.periodic(Duration(seconds: 5), (nada) {
       if(fecha == _dateSelected.day.toString() + '-' + _dateSelected.month.toString() + '-' +   _dateSelected.year.toString()){
         _showAlertDialog();
       }
-    });
+    });*/
   }
 
   @override
@@ -112,7 +111,7 @@ class _MiHomeState extends State<MiHome> {
               final item = misNotas[index];
               return Container(
                 height: MediaQuery.of(context).size.height / 7,
-                child: CardLista(Colors.blue,index,this._recordatorio),
+                child: CardLista(colr, index, this._recordatorio),
               );
             },
           ),
@@ -121,7 +120,8 @@ class _MiHomeState extends State<MiHome> {
       ],
     );
   }
-  Widget CardLista(Color color , int index, Recordatorio record){
+
+  Widget CardLista(Color color, int index, Recordatorio record) {
     return Card(
         color: color,
         elevation: 3.0,
@@ -132,15 +132,14 @@ class _MiHomeState extends State<MiHome> {
               builder: (BuildContext context) {
                 return AlertDialog(
                   title: const Text("Clonar Nota"),
-                  content: const Text(
-                      "Desea Clonar la nota con los mismos datos?"),
+                  content:
+                      const Text("Desea Clonar la nota con los mismos datos?"),
                   actions: <Widget>[
                     FlatButton(
                         onPressed: () {
                           setState(() {
                             record = misNotas[index];
-                            Recordatorio rec =
-                            record.clonar();
+                            Recordatorio rec = record.clonar();
                             misNotas.add(rec);
                           });
                           Navigator.of(context).pop(true);
@@ -166,17 +165,14 @@ class _MiHomeState extends State<MiHome> {
                   width: MediaQuery.of(context).size.width,
                   child: Text(
                     misNotas[index].getTitulo,
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold),
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                   ),
                 ),
                 Container(
-                  padding: EdgeInsets.only(
-                      top: 10.0, left: 15.0, right: 15.0),
+                  padding: EdgeInsets.only(top: 10.0, left: 15.0, right: 15.0),
                   child: Row(
-                    mainAxisAlignment:
-                    MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(misNotas[index].getContenido),
                       Text(misNotas[index].getFechaHora),
@@ -188,6 +184,7 @@ class _MiHomeState extends State<MiHome> {
           ),
         ));
   }
+
   Widget _crearBoton() {
     return Container(
         padding: const EdgeInsets.only(top: 20.0),
@@ -237,7 +234,7 @@ class _MiHomeState extends State<MiHome> {
                               borderRadius:
                                   BorderRadius.all(Radius.circular(10.0)),
                               color: Colors.pink[300]),
-                            child: Center(
+                          child: Center(
                             child: Text("Guardar"),
                           ),
                         ),
@@ -266,60 +263,91 @@ class _MiHomeState extends State<MiHome> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          colorsitos(Colors.red,"Rojo"),
-          colorsitos(Colors.blue,"Azul"),
-          colorsitos(Colors.yellow,"Amarillo"),
-          colorsitos(Colors.green,"Verde"),
-          colorsitos(Colors.pink,"Rosa"),
+          GestureDetector(
+              onTap: () {
+                setState(() {
+                  colr = getColorAplicandoEstrategia(new ColorRojo());
+                });
+              },
+              child: colorsitos(Colors.red)),
+          GestureDetector(
+              onTap: () {
+                setState(() {
+                  colr = getColorAplicandoEstrategia(new ColorAzul());
+                });
+              },
+              child: colorsitos(Colors.blue)),
+          GestureDetector(
+              onTap: () {
+                setState(() {
+                  colr = getColorAplicandoEstrategia(new ColorAmarillo());
+                });
+              },
+              child: colorsitos(Colors.yellow)),
+          GestureDetector(
+              onTap: () {
+                setState(() {
+                  colr = getColorAplicandoEstrategia(new ColorVerde());
+                });
+              },
+              child: colorsitos(Colors.green)),
+          GestureDetector(
+              onTap: () {
+                setState(() {
+                  colr = getColorAplicandoEstrategia(new ColorRojo());
+                });
+              },
+              child: colorsitos(Colors.pink)),
         ],
       ),
     );
   }
 
-  Color crearColor(String color) {
+  getColorAplicandoEstrategia(ColorEstrategy estrategy) {
+    return estrategy.getColor();
+  }
+
+  /*Color crearColor(String color) {
     Color colorsitodelista;
     switch (color) {
       case "Azul":
         {
-          colorsitodelista= getColorAplicandoEstrategia(new ColorAzul());
+          colorsitodelista = getColorAplicandoEstrategia(new ColorAzul());
         }
         break;
 
       case "Amarillo":
         {
-          colorsitodelista= getColorAplicandoEstrategia(new ColorAzul());
+          colorsitodelista = getColorAplicandoEstrategia(new ColorAzul());
         }
         break;
       case "Verde":
         {
-          colorsitodelista= getColorAplicandoEstrategia(new ColorAzul());
+          colorsitodelista = getColorAplicandoEstrategia(new ColorAzul());
         }
         break;
       case "Rosa":
         {
-          colorsitodelista= getColorAplicandoEstrategia(new ColorAzul());
+          colorsitodelista = getColorAplicandoEstrategia(new ColorAzul());
         }
         break;
       case "Rojo":
         {
-          colorsitodelista= getColorAplicandoEstrategia(new ColorAzul());
+          colorsitodelista = getColorAplicandoEstrategia(new ColorAzul());
         }
         break;
       default:
         break;
     }
     return colorsitodelista;
-  }
+  }*/
 
-  Widget colorsitos(Color color, String text) {
-    return GestureDetector(
-      onTap: () {crearColor(text);},
-      child: Container(
-        height: 30.0,
-        width: 30.0,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50.0), color: color),
-      ),
+  Widget colorsitos(Color color) {
+    return Container(
+      height: 30.0,
+      width: 30.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50.0), color: color),
     );
   }
 
